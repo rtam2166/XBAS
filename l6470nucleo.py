43129e742afd733e53109b8ce9332510a298b6cb
rtam2166 <rtam2166@gmail.com>
2017-05-22 12:30:06 -0700

    Update l6470nucleo.py
    Added authors

--- l6470nucleo.py
+++ l6470nucleo.py
@@ -9,7 +9,8 @@
 #  @e etc. need to be tuned for that motor using the methods shown at
 #  @c http://www.st.com/resource/en/application_note/dm00061093.pdf. 
 #
-#  @author JR Ridgely, etc etc
+#  @author John Ridgely, John Barry, Anthony Lombardi, 
+#           Whittaker Hamill, and Sam Artho-Bentz
 
 
 import pyb
